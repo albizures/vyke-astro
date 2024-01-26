504cf032955ace79b39f56265642e76241a7deda
Jose Albizures <jose@albizures.com>
2024-01-25 21:09:31 -0600

    refactor: move all component inside of src

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// @ts-expect-error component is untyped unless loaded by language tools
+export { default as Page404 } from "./components/404.astro";
+// @ts-expect-error component is untyped unless loaded by language tools
+export { default as Footer } from "./components/footer.astro";
+// @ts-expect-error component is untyped unless loaded by language tools
+export { default as IconLinks } from "./components/icon-links.astro";
