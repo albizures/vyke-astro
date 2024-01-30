398a2947383b3a2d49f17da329b3000119597291
Jose Albizures <jose@albizures.com>
2024-01-29 20:43:37 -0600

    feat: add header, section and theme controller components

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,9 @@ export { default as Page404 } from "./components/404.astro";
 export { default as Footer } from "./components/footer.astro";
 // @ts-expect-error component is untyped unless loaded by language tools
 export { default as IconLinks } from "./components/icon-links.astro";
+// @ts-expect-error component is untyped unless loaded by language tools
+export { default as Header } from "./components/header.astro";
+// @ts-expect-error component is untyped unless loaded by language tools
+export { default as ThemeController } from "./components/theme-controller.astro";
+// @ts-expect-error component is untyped unless loaded by language tools
+export { default as Section } from "./components/section.astro";
