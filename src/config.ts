36c2b0af6e0ad6f52f3ab69b742fefd1fcf4042a
Jose Albizures <jose@albizures.com>
2024-02-08 09:30:03 -0600

    feat: create theme and theme toggle to handle themes

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const config = {
+	theme: {
+		dark: "dark",
+		light: "cupcake",
+	},
+};
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
+export { config } from './config'
+
 // @ts-expect-error component is untyped unless loaded by language tools
-export { default as Page404 } from "./components/404.astro";
+export { default as Page404 } from './components/404.astro'
 // @ts-expect-error component is untyped unless loaded by language tools
-export { default as Footer } from "./components/footer.astro";
+export { default as Footer } from './components/footer.astro'
 // @ts-expect-error component is untyped unless loaded by language tools
-export { default as IconLinks } from "./components/icon-links.astro";
+export { default as IconLinks } from './components/icon-links.astro'
 // @ts-expect-error component is untyped unless loaded by language tools
-export { default as Header } from "./components/header.astro";
+export { default as Header } from './components/header.astro'
+export {
+	/**
+	 * @deprecated
+	 */
+	default as ThemeController,
+	// @ts-expect-error component is untyped unless loaded by language tools
+} from './components/theme-controller.astro'
 // @ts-expect-error component is untyped unless loaded by language tools
-export { default as ThemeController } from "./components/theme-controller.astro";
+export { default as Section } from './components/section.astro'
 // @ts-expect-error component is untyped unless loaded by language tools
-export { default as Section } from "./components/section.astro";
+export { default as ThemeToggle } from './components/theme-toggle.astro'
+// @ts-expect-error component is untyped unless loaded by language tools
+export { default as Theme } from './components/theme.astro'
